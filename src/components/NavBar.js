718f921a3076c224acee9af5fc95082700be0e04
Faith Kariuki <tcwamboi@gmail.com>
2024-08-06 13:31:05 +0300

    Complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,11 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/directors">Directors</NavLink>
+        <NavLink to="/actors">Actors</NavLink>
+      </nav>
     </nav>
     );
 };
--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,13 +1,32 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+    .then(res => res.json())
+    .then(data => setDirectors(data))
+    .then(error => console.log(error))
+  }, [])
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directors && directors.map(director => (
+          <article key={director.id}>
+            <h2>{director.name}</h2>
+            <ul>
+              <li>
+              {director.movies}
+              </li>
+            </ul>
+          </article>
+        ))}
       </main>
     </>
   );
